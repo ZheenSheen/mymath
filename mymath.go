935a131e6c64657e622b7f15b7659f1251eeb38e
Eugene Shamshin <jenkatop1intheworld@gmail.com>
2024-09-10 18:23:03 +0500

    1

--- mymath.go
+++ mymath.go
@@ -2,28 +2,25 @@ package mymath
 
 import "math"
 
-func Abs(x float64) float64 {
-    if x < 0 {
-        return -x
-    } else {
-        return x
-    }
-}
-
-func Max(a, b float64) float64 {
-    if a > b {
-        return a
-    } else {
-        return b
-    }
-}
-
 func Sqrt(x float64) float64 {
     return math.Sqrt(x)
 }
 
-func Yn(m, n int) float64 {
-    result := m * n
-    return float64(result)
+func Add(a, b float64) float64 {
+    return a + b
+}
 
+func Subtract(a, b float64) float64 {
+    return a - b
+}
+
+func Multiply(a, b float64) float64 {
+    return a * b
+}
+
+func Divide(a, b float64) float64 {
+    if b != 0 {
+        return a / b
+    }
+    return math.NaN()
 }
